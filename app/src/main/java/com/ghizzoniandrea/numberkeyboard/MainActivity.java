e68543866d06acbf430bebf9ff402822a8e1279a
shangxiazuoyou <megzzi@163.com>
2018-06-06 17:05:10 +0800

    修复bug

--- app/src/main/java/com/ghizzoniandrea/numberkeyboard/MainActivity.java
+++ app/src/main/java/com/ghizzoniandrea/numberkeyboard/MainActivity.java
@@ -13,52 +13,46 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private NumberKeyboard mKeyboard;
+    private EditText edtTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText edtTest = (EditText) findViewById(R.id.edt_test);
-        edtTest.setOnFocusChangeListener(new android.view.View.OnFocusChangeListener() {
+        edtTest = (EditText) findViewById(R.id.edt_test);
+    }
 
+    public void show(View view) {
+        mKeyboard = new NumberKeyboard(MainActivity.this, new OnKeyActionListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    // 获得焦点
-                    mKeyboard = new NumberKeyboard(MainActivity.this, new OnKeyActionListener() {
-                        @Override
-                        public void onFinish(int input) {
-                            edtTest.setText(Character.toString((char) input));
-
-                        }
-
-                        @Override
-                        public void onProcess(int input) {
-                            Editable editable = edtTest.getText();
-                            int start = edtTest.getSelectionStart();
-                            if (input == Keyboard.KEYCODE_DELETE) {// 回退
-                                if (editable != null && editable.length() > 0) {
-                                    if (start > 0) {
-                                        editable.delete(start - 1, start);
-                                    }
-                                }
-                            } else if (input == 0) {// 无操作
-
-                            } else {
-                                editable.insert(start, Character.toString((char) input));
-                            }
-                        }
-                    });
-                    mKeyboard.show(MainActivity.this.getWindow().getDecorView().getRootView());
-                } else {
-                    // 失去焦点
-                    mKeyboard.dismiss();
-                }
-
+            public void onFinish(int input) {
+                edtTest.setText(Character.toString((char) input));
             }
 
+            @Override
+            public void onProcess(int input) {
+                Editable editable = edtTest.getText();
+                int start = edtTest.getSelectionStart();
+                if (input == Keyboard.KEYCODE_DELETE) {// 回退
+                    if (editable != null && editable.length() > 0) {
+                        if (start > 0) {
+                            editable.delete(start - 1, start);
+                        }
+                    }
+                } else if (input == 0) {// 无操作
 
+                } else {
+                    editable.insert(start, Character.toString((char) input));
+                }
+            }
         });
+        mKeyboard.show(MainActivity.this.getWindow().getDecorView().getRootView());
+    }
+
+    public void dismiss(View view) {
+        if (null != mKeyboard) {
+            mKeyboard.dismiss();
+        }
     }
 }
